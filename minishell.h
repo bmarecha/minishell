c752839b41ddfd83585bea240ac8a9929e03e0ce
bmarecha <baptistemarechal42@gmail.com>
2022-01-06 20:13:56 +0100

    Redir and removing piping folder

--- minishell.h
+++ minishell.h
@@ -1,3 +1,12 @@
+
+//type of redir : 1 == '<', 2 == '<<', 3 == '>>', 4 == '>'
+
+typedef struct s_redir
+{
+	char	*file;
+	int		type;
+}
+
 /*
 ** pipe : 0 == entree, 1 == sortie, 2 == sortie et entree, 3 == aucun des deux
 ** next et/ou prev sont mis a NULL dans les cas correspondants
@@ -9,10 +18,10 @@ typedef struct s_cmd
 	struct s_cmd	*next;
 	struct s_cmd	*prev;
 	
-	char			*cmd;
+	char			*name;
 	char			**argv;
 	int				pipe;
 
-//	t_redir			*i_red;
-//	t_redir			*o_red;
+	t_redir			*i_red;
+	t_redir			*o_red;
 }					t_cmd;
